c8852658551d0c19899200c689955e4011f20e37
jojurg <jojurg@taltech.com>
2023-11-12 22:05:29 +0200

    implementing functions for App

--- src/app.rs
+++ src/app.rs
@@ -9,4 +9,24 @@ pub enum CurrentlyEditing { // to keep track of which field is being currently e
     Value,
 }
 
+pub struct App {
+    pub key_input: String,                  // the currently being edited json key
+    pub value_input: String,                // the currently being edited json value
+    pub pairs: HashMap<String, String>,     // representation of our key-value pairs with serde serialization support
+    pub current_screen: CurrentSreen,       // current screen the user is looking at, will determine what is rendered
+    pub currently_editing: CurrentlyEditing // the optional state containing which of the key or value pairs the user is editing
+}
 
+impl App {
+    pub fn new() -> App {
+        App {
+            key_input: String::new(),
+            value_input: String::new(),
+            pairs: HashMap::new(),
+            current_screen: CurrentSreen::Main,
+            currently_editing: None,
+        }
+    }
+
+
+}
