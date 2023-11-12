7be3672eea6e3972af8f9f2971ba3e976a63ec9f
jojurg <jojurg@taltech.com>
2023-11-12 21:58:33 +0200

    struct CurrentlyEditing

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,12 @@
-pub enum CurrentSreen {
+pub enum CurrentSreen { // to keep track of what screen is being displayed
     Main,
     Editing,
     Exiting,
 }
+
+pub enum CurrentlyEditing { // to keep track of which field is being currently edited
+    Key,
+    Value,
+}
+
+
